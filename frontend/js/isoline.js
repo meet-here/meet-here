3661f3e4affd5692f9e8fdd3c393be02e9f0594c
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2015-06-12 17:20:19 +0200

    add isolined markers

--- frontend/js/isoline.js
+++ frontend/js/isoline.js
@@ -1,4 +1,7 @@
-function drawIsoline(map, point) {
+isolines = []
+isolineObjects = []
+
+function makeIsoline(map, point, timeframe) {
   $.ajax({
     url: 'https://route.st.nlp.nokia.com/routing/6.2/calculateisoline.json',
     type: 'GET',
@@ -7,13 +10,24 @@ function drawIsoline(map, point) {
     data: {
       mode: 'fastest;car',
       start: point.lat.toString()+","+point.lng.toString(),
-      time: 'PT0H05M',
+      time: 'PT0H'+timeframe.toString()+'M',
       app_id: 'DemoAppId01082013GAL',
       app_code: 'AJKnXv84fjrb0KIHawS0Tg'
     },
     success: function (data) {
-      addPolygonToMap(map,data.Response.isolines[0].value);
+      isolines.push(data.Response.isolines[0].value);
+      // redrawIsolines(map)
+      addPolygonToMap(map, data.Response.isolines[0].value);
     }
+  });
+}
+
+function redrawIsolines(map) {
+  $(isolineObjects).each(function (index, elem) {
+    map.removeObject(elem)
+  });
+  $(isolines).each (function (index, elem) {
+    addPolygonToMap(map,elem);
   });
 }
 
@@ -24,13 +38,12 @@ function addPolygonToMap(map, polygon) {
     var point = new H.geo.Point(parseFloat(coords[0]), parseFloat(coords[1]));
     geoStrip.pushPoint(point);
   };
-  map.addObject(
-    new H.map.Polygon(geoStrip, {
-      style: {
-        fillColor: '#FFFFCC',
-        strokeColor: '#829',
-        lineWidth: 8
-      }
-    })
-  );
+  var polygonObj = new H.map.Polygon(geoStrip, {
+    style: {
+      strokeColor: '#829',
+      lineWidth: 2
+    }
+  })
+  // map.addObject(polygonObj);
+  isolineObjects.push(map.addObject(polygonObj));
 }
