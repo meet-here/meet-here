a350b44d85b8d3a249894f762e44b7d5c7feca22
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2015-06-12 14:23:25 +0200

    change color of markers

--- frontend/mapstuff.js
+++ frontend/mapstuff.js
@@ -1,4 +1,5 @@
 var markers = [];
+var user_marker = '<svg xmlns="http://www.w3.org/2000/svg" width="28px" height="36px"><path d="M 19 31 C 19 32.7 16.3 34 13 34 C 9.7 34 7 32.7 7 31 C 7 29.3 9.7 28 13 28 C 16.3 28 19 29.3 19 31 Z" fill="#000" fill-opacity=".2"/><path d="M 13 0 C 9.5 0 6.3 1.3 3.8 3.8 C 1.4 7.8 0 9.4 0 12.8 C 0 16.3 1.4 19.5 3.8 21.9 L 13 31 L 22.2 21.9 C 24.6 19.5 25.9 16.3 25.9 12.8 C 25.9 9.4 24.6 6.1 22.1 3.8 C 19.7 1.3 16.5 0 13 0 Z" fill="#fff"/><path d="M 13 2.2 C 6 2.2 2.3 7.2 2.1 12.8 C 2.1 16.1 3.1 18.4 5.2 20.5 L 13 28.2 L 20.8 20.5 C 22.9 18.4 23.8 16.2 23.8 12.8 C 23.6 7.07 20 2.2 13 2.2 Z" fill="{FILL}"/></svg>';
 
 $(document).ready(function(){
 
@@ -20,6 +21,7 @@ $(document).ready(function(){
           center: { lat: 52.5, lng: 13.4 }
         });
     var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    var ui = H.ui.UI.createDefault(map, defaultLayers);
 
     setUpClickListener(map, behavior);
     addMarkersToMap(map);
@@ -31,9 +33,11 @@ function setUpClickListener(map, behavior) {
   map.addEventListener('tap', function (evt) {
     var coord = map.screenToGeo(evt.currentPointer.viewportX,
             evt.currentPointer.viewportY);
- 
+
+    var icon = new H.map.Icon(user_marker.replace("{FILL}", "red"));
     var marker = new H.map.Marker({lat: coord.lat, lng: coord.lng});
     marker.draggable = true;
+    marker.setIcon(icon);
 
     // disable the default draggability of the underlying map
     // when starting to drag a marker object:
@@ -71,13 +75,22 @@ function setUpClickListener(map, behavior) {
 }
 
 function addMarkersToMap(map) {
-    var marker1 = new H.map.Marker({lat: 52.5411, lng: 13.394});
-    var marker2 = new H.map.Marker({lat: 52.5193, lng: 13.39828});
-    var marker3 = new H.map.Marker({lat: 52.53379, lng: 13.394163});
-    var marker4 = new H.map.Marker({lat: 52.5216785, lng: 13.408497});
+    var positions = [
+        {lat: 52.5411, lng: 13.394},
+        {lat: 52.5193, lng: 13.39828},
+        {lat: 52.53379, lng: 13.394163},
+        {lat: 52.5216785, lng: 13.408497}
+    ];
+
+    $(positions).each(function (index, element){        
+        var marker = new H.map.Marker(element);
+        var icon = new H.map.Icon(user_marker.replace("{FILL}", "red"));
+        marker.setIcon(icon);
+        markers.push(marker);
+    });
 
     var group = new H.map.Group();
-    group.addObjects([marker1, marker2, marker3, marker4]);
+    group.addObjects(markers);
     map.addObject(group);
     map.setViewBounds(group.getBounds());
 }
